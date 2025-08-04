f9fe8b3ad3d4fa20905fda9e05b7ea7d6c7c811d
LQDias <leo_dias2005@yahoo.com.br>
2025-08-03 22:23:41 -0300

    Exception

---
+++ src/exceptions/NenhumaDificuldadeSelecionada.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class NenhumaDificuldadeSelecionada extends Exception {
+    public NenhumaDificuldadeSelecionada() {
+        super("Selecione uma dificuldade para iniciar o jogo");
+    }
+}
